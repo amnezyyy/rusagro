af28419ac9b1b0a147e7956764dd0b22a49685c7
amnezyyy <fomin.casch@yandex.ru>
2023-06-25 19:30:53 +0300

    init

--- app/Console/Commands/TestCommand.php
+++ app/Console/Commands/TestCommand.php
@@ -38,10 +38,6 @@ class TestCommand extends Command
      */
     public function handle()
     {
-        try {
-            throw new Exception('Сообщение об ошибке', 123);
-        } catch (Exception $e) {
-            echo 'Было поймано исключение: ' . $e->getMessage() . '. Код: ' . $e->getCode();
-        }
+        echo '12312132';
     }
 }
